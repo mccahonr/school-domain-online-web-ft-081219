33ba0fe3c414d4aeddb267a0dd944407eb529c4a
Ryan McCahon <mccahonr@yahoo.com>
2019-08-20 18:24:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader
+
+  def initialize(roster)
+    @roster = {}
+  end
+  def add_student(student,year)
+    roster[year] ||= []
+    roster[year] << student
+  end
+  def grade(year)
+    roster.detect do |x, y|
+      if x == year
+         return y
+      end
+    end
+  end
+  def sort
+    sorted_students = {}
+    roster.each do |x, y|
+      sorted_students[x] = y.sort
+    end
+    sorted_students
+  end
+
+
+end
